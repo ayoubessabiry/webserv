b3ba6cdd87d4ce87ad0891df0b44a58f76b6d37d
Ayoub Essabiry <aessabir@e2r8p9.1337.ma>
2023-06-11 13:31:17 +0100

    creating websrv.hpp

---
+++ webserv.hpp
@@ -0,0 +1,16 @@
+#ifndef WEBSERV_GARD
+# define WEBSERV_GARD
+
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <netdb.h>
+# include <unistd.h>
+# include <iostream>
+# include <cctype>
+# include <ctime>
+# define PORT "8080"
+# define BACKLOG 10
+
+#endif
