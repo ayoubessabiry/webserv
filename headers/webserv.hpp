128d854dfea225b022d217dc22c47a786f0cd231
Ayoub Essabiry <aessabir@e2r8p8.1337.ma>
2023-07-07 10:18:51 +0100

    merging

--- headers/webserv.hpp
+++ headers/webserv.hpp
@@ -3,20 +3,25 @@
 
 # include <sys/types.h>
 # include <sys/socket.h>
+# include <sys/time.h>
 # include <netinet/in.h>
 # include <arpa/inet.h>
 # include <netdb.h>
 # include <unistd.h>
+# include <string.h>
 # include <iostream>
 # include <cctype>
 # include <ctime>
 # include <vector>
 # include <cstring>
 # include <string>
+# include <cstdlib>
 # include <cstdio>
 # include <fstream>
-# define PORT "8080"
-# define BACKLOG 10
+# include <map>
+#include "../request/parse_request.hpp"
+
+# define BACKLOG  32767
 
 bool	send_request(char	*buff);
 
