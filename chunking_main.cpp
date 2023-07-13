3585bbaf1173153a36f606c9aa4b409ea4156492
Leonidasabiri <sabirim52@gmail.com>
2023-07-13 20:20:02 +0100

    Chunking maybe working

--- chunking_main.cpp
+++ chunking_main.cpp
@@ -1,7 +1,6 @@
 
 # include "headers/Init.hpp"
 
-
 int	covert_hex_to_decimal(std::string hex_number)
 {
 	int					num;
@@ -13,10 +12,12 @@ int	covert_hex_to_decimal(std::string hex_number)
 	return num;
 }
 
-bool test(std::string &req, Client client)
+bool test(std::string &req, Client &client)
 {
 	int	body_chunk_size = req.size();
-	std::cout << body_chunk_size << std::endl;
+
+    if (!client.rqst.found_next_hexa)
+        client.rqst.next_hex_saver = req;
 
 	client.rqst.is_reading_new_chunk_part = true;
 	while(body_chunk_size)
@@ -24,46 +25,53 @@ bool test(std::string &req, Client client)
 		// Search for first hexa
 		if (!client.rqst.found_next_hexa)
 		{
-			if (!strstr(req.c_str(), "\r\n"))
+            std::string chunk_part = client.rqst.next_hex_saver;
+			if (!strstr(chunk_part.c_str(), "\r\n"))
 				return false;
-			std::size_t find = req.find("\r\n");
-			client.rqst.next_hex_saver += req.substr(0, find);
-			client.rqst.found_next_hexa = true;
+			std::size_t find = client.rqst.next_hex_saver.find("\r\n");
+			client.rqst.next_hex_saver = client.rqst.next_hex_saver.substr(0, find);
+            client.rqst.found_next_hexa = true;
 			client.rqst.chunk_size = covert_hex_to_decimal(client.rqst.next_hex_saver);
-			client.rqst.chunk_saver = req.substr(find + 2, req.size());
+			if (client.rqst.chunk_size == 0)
+			{
+				client.rqst.found_next_hexa = false;
+				return true ;
+			}
+            client.rqst.chunk_saver = chunk_part.substr(find + 2, chunk_part.size());
+            client.rqst.is_reading_new_chunk_part = false;
 			body_chunk_size -= client.rqst.next_hex_saver.size();
 			body_chunk_size -= 2;
 		}
 		// Proceed to find other hexas to convert
 		if (client.rqst.found_next_hexa)
 		{
-			if (client.rqst.chunk_size == 0)
-			{
-				client.rqst.found_next_hexa = false;
-				return true ;
-			}
-			if (client.rqst.is_reading_new_chunk_part)
-			{
-				client.rqst.chunk_saver += req;
-				client.rqst.is_reading_new_chunk_part = false;
-			}
-			if (client.rqst.chunk_saver.size() > client.rqst.chunk_size)
-			{
-				std::string chunk = client.rqst.chunk_saver;
-				client.rqst.next_hex_saver = chunk.substr(client.rqst.chunk_size, client.rqst.chunk_saver.size());
-				client.rqst.chunk_saver = chunk.substr(0, client.rqst.chunk_size);
-				body_chunk_size -= client.rqst.chunk_saver.size();
-			}
-			client.rqst.found_next_hexa = !(client.rqst.chunk_saver.size() >= client.rqst.chunk_size);
-
 			std::fstream	body_file;
 			body_file.open("test", 
 							std::ios_base::binary|std::ios_base::out|
 							std::ios_base::app);
-			body_file << client.rqst.chunk_saver;
+
+			if (client.rqst.is_reading_new_chunk_part)
+			{
+				client.rqst.chunk_saver += req;
+                client.rqst.is_reading_new_chunk_part = false;
+            }
+			if (client.rqst.chunk_saver.size() >= client.rqst.chunk_size)
+			{
+				std::string chunk = client.rqst.chunk_saver;
+				client.rqst.next_hex_saver = chunk.substr(client.rqst.chunk_size, client.rqst.chunk_saver.size());
+                client.rqst.chunk_saver = chunk.substr(0, client.rqst.chunk_size);
+                std::cout << client.rqst.chunk_saver << "\n\n";
+                body_file << client.rqst.chunk_saver;
+                body_chunk_size -= client.rqst.chunk_saver.size();
+			}
+            else
+                body_chunk_size -= client.rqst.chunk_saver.size();
+			
+            client.rqst.found_next_hexa = !(client.rqst.chunk_saver.size() >= client.rqst.chunk_size);
+
+            if (body_chunk_size <= 0)
+                body_chunk_size = 0;
 		}
-		if (body_chunk_size <= 0)
-			body_chunk_size = 0;
 	}
 
 	return false;
@@ -71,41 +79,20 @@ bool test(std::string &req, Client client)
 
 int	main(int ac, char **av)
 {
-	std::ifstream 		file;
-	std::stringstream	file_data;
-	std::string			file_data_string;
-	Init				websrv;
-	webserver			web_s;
-
-	file.open(av[1]);
-
-	file_data << file.rdbuf();
-
-	file_data_string = file_data.str();
-
-	web_s.parse_server_block(file_data_string);
-	// web_s.print_config_file();
-
-	// for(int i = 0; i < web_s.server_blocks.size(); ++i){
-	// 	Server s(web_s.server_blocks[i]);//web_s.server_blocks[i].port.c_str(), web_s.server_blocks[i].host.c_str(), web_s.server_blocks[i].server_name);
-	// 	websrv.add_server(s);
-	// }
-	// websrv.start_listening();
-
 	Client client;
 
-	
 	client.rqst.next_hex_saver = "";
+	client.rqst.found_next_hexa = false;
 
 	std::string	chunked_body =  
 	"C\r\n"
 	"Some data..."
 	"11\r\n"
-	"Some more data..."
+	"Sope more data..."
+    "A\r\n"
+    "ponponponp"
 	"0\r\n";
 
-	std::cout << test(chunked_body, client);
+    std::string o = chunked_body.substr(0, 16);
+    test(chunked_body, client);
 }
-
-
-
