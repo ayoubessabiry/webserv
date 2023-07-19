d20403794e1a358388e6d9cf3fbe071568b15b8c
Mounir Sabiri <sabirim52@gmail.com>
2023-07-19 19:39:31 +0100

    fixed location matching

--- Client.cpp
+++ Client.cpp
@@ -16,20 +16,21 @@ location_block Client::match_location()
 {
     int     location_index = 0;
     int     matching_size = 0, max_matching_size = -1;
+    bool    match_none = false;
 
+    if (rqst.uri == "/")
+        return configuration.locations[location_index];
     for (int i = 0; i < configuration.locations.size(); i++)
     {
         if (configuration.locations[i].prefix == "/")
             i++;
-        for (int j = 0; i < configuration.locations.size()
-            && j < configuration.locations[i].prefix.size()
-            && j < rqst.uri.size()
-            && rqst.uri[j] == configuration.locations[i].prefix[j]; j++)
+        for (int j = 1; rqst.uri[j] == configuration.locations[i].prefix[j]
+            && rqst.uri >= configuration.locations[i].prefix ; j++)
         {
             if (configuration.locations[i].prefix[j] == '/')
                 matching_size++;
         }
-        if (max_matching_size < matching_size)
+        if (max_matching_size < matching_size && matching_size > 0)
         {
             max_matching_size = matching_size;
             location_index = i;
